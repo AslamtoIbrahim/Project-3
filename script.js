c66de9a5b6f4519efcb00b014622eaf8a1a9ff90
Aslamto Ibrahim <aslamtoibrahim@gmail.com>
2023-07-31 09:24:56 +0000

    I finshed js code and html connections

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 
 
-const one = document.getElementById("one");
+ 
+var rate = 0;
+function setRate(num){
+    rate = num;
+    console.log(rate);
+}
 
-one.addEventListener('clcik' , function(){
-    const value = one.ariaValueText;
-    console.log(value);
-});
+function submit(){
+    var div1 = document.getElementById("div1");
+    var div2 = document.getElementById("div2");
+    var rateElement = document.getElementById("rate");
+    div1.style.display = "none";
+    div2.style.display = "flex";
+    rateElement.innerHTML = rate; 
+}
+
+function rateAgain(){
+    var div1 = document.getElementById("div1");
+    var div2 = document.getElementById("div2");
+    var rateElement = document.getElementById("rate");
+    div1.style.display = "flex";
+    div2.style.display = "none";
+    rate = 0;
+    rateElement.innerHTML = rate; 
+}
